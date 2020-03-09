34c0dee8bfe4e58495d01a2a57529b7f9562f27f
Santiago Torres <santiagotorresredecilla@gmail.com>
2020-03-09 16:02:52 +0200

    wip

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,31 +1,33 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (let [check (fn [x] (or (empty? x) (nil? x) (every? whitespace? x)))]
+    (check string)))
 
 (defn has-award? [book award]
-  :-)
+  (let [book-awd-check (fn [b a] (filter (key->predicate :awards) b))]
+    (book-awd-check book award)))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   :-)
